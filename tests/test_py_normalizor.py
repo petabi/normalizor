a6bd96e888609951025a6d9bdd2ed4b9bba6aee7
vvalgenti <vvalgenti@petabi.com>
2018-06-28 13:08:12 -0700

    Minor check in py script.

--- tests/test_py_normalizor.py
+++ tests/test_py_normalizor.py
@@ -9,6 +9,10 @@ def main():
     myln = norm.Line_normalizer()
     myln.set_input_stream(filename)
     mylines = myln.normalize()
+    for l in mylines:
+        if len(l.sections) == 0:
+            return 1
+
 
 if __name__ == "__main__":
     main()

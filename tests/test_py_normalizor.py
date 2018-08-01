eb3c424d603469415a9c73f3c32da2bbdec0b6ce
vvalgenti <vvalgenti@petabi.com>
2018-07-31 17:45:07 -0700

    look for local libs first in test.

--- tests/test_py_normalizor.py
+++ tests/test_py_normalizor.py
@@ -4,7 +4,7 @@ import sys
 def main():
     libdir = sys.argv[1]
     filename = sys.argv[2]
-    sys.path.append(libdir)
+    sys.path.insert(0, libdir)
     import py_normalizor as norm
     myln = norm.Line_normalizer()
     myln.set_input_stream(filename)
